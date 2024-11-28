83286e45624e9553f35c1da8c2df58edfa38e6bf
ZackFra <zack11frazier22@gmail.com>
2024-11-27 21:23:46 -0800

    Add error handling for job validation failure in get-job-id script

--- scripts/js/get-job-id.js
+++ scripts/js/get-job-id.js
@@ -3,4 +3,7 @@ const fs = require("fs");
 const validateJob = JSON.parse(fs.readFileSync("validate.json", "utf8"));
 if(validateJob.success) {
     fs.writeFileSync("job-id.txt", validateJob.id, "utf8");
+} else {
+    console.error("Job failed to validate");
+    process.exit(1);
 }
