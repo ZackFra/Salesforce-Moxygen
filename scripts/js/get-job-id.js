2ed385a37c24b81d89aca536204d89b8a448eb60
ZackFra <zack11frazier22@gmail.com>
2024-11-27 21:57:58 -0800

    Refactor job ID retrieval to use streams and improve error handling for validation parsing

--- scripts/js/get-job-id.js
+++ scripts/js/get-job-id.js
@@ -1,9 +1,31 @@
 const fs = require("fs");
 
-const validateJob = JSON.parse(fs.readFileSync("validate.json", "utf8"));
-if(validateJob.success) {
-    fs.writeFileSync("job-id.txt", validateJob.id, "utf8");
-} else {
-    console.error("Job failed to validate");
-    fs.writeFileSync("job-id.txt", "", "utf8");
+const fileStream = fs.createReadStream("validate.json", "utf-8");
+let validateJobText = "";
+
+fileStream.on("data", (chunk) => {
+    validateJobText += chunk.toString('utf-8');
+});
+
+fileStream.on("end", () => {
+    try {
+        parseValidateJob(validateJobText);
+    } catch (err) {
+        console.error("Error parsing validate job: ", err);
+        fs.writeFileSync("job-id.txt", "", "utf-8");
+    }
+});
+
+const parseValidateJob = (validateJobText) => {
+    const validateJob = JSON.parse(removeNonReadable(validateJobText));
+    if(validateJob.result.success) {
+        fs.writeFileSync("job-id.txt", validateJob.result.id, "utf-8");
+    } else {
+        console.error("Job failed to validate");
+        fs.writeFileSync("job-id.txt", "", "utf-8");
+    }
+}
+
+const removeNonReadable = (str) => {
+    return str.replace(/[^\x20-\x7E]/g, '');
 }
