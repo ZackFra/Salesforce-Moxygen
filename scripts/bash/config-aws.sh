ea0903ac76f1accbe382e3b23a0b97e27f58e026
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 17:31:30 -0800

    Add shebang lines to AWS scripts for improved compatibility

--- scripts/bash/config-aws.sh
+++ scripts/bash/config-aws.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 npm i -g aws-cdk
 aws configure set aws_access_key_id $AWS_ACCESS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET
--- scripts/bash/invoke-lambda-func.sh
+++ scripts/bash/invoke-lambda-func.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 aws lambda invoke --payload $1 --function-name $2 --cli-binary-format raw-in-base64-out outfile.json 
