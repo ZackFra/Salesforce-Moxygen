62e4cee7b7775f75d86c71fe3086c7e339099f27
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 17:30:56 -0800

    Fix shebang line in AWS scripts and remove unnecessary lines

--- scripts/bash/config-aws.sh
+++ scripts/bash/config-aws.sh
@@ -1,5 +1,3 @@
-!#/bin/bash
-
 npm i -g aws-cdk
 aws configure set aws_access_key_id $AWS_ACCESS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET
--- scripts/bash/invoke-lambda-func.sh
+++ scripts/bash/invoke-lambda-func.sh
@@ -1,2 +1,1 @@
-!#/bin/bash
 aws lambda invoke --payload $1 --function-name $2 --cli-binary-format raw-in-base64-out outfile.json 
