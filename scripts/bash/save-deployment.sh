617216997b59e241e22594cca0e520ff5cde9a7a
Zackary Frazier <52726500+ZackFra@users.noreply.github.com>
2025-01-04 20:48:30 -0800

    Revert "[FEATURE] NEXT_N_QUARTERS (#130)"
    This reverts commit a49ce0fb6588672263e29bc980f39248e50f0176.

--- scripts/bash/save-deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-scripts/bash/config-aws.sh
-node scripts/js/format-deployment-data.js "validate.json"
-scripts/bash/invoke-lambda-func.sh "file://deployment-request.json" $AWS_SAVE_DEPLOYMENT_FUNC
