acf196a7cfb42b06e60d564406c40814f99a614f
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 18:53:33 -0800

    Fix get-deployment script to output to outfile.json instead of found-deployment.json

--- scripts/bash/get-deployment.sh
+++ scripts/bash/get-deployment.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-scripts/bash/invoke-lambda-func.sh "{\"pullRequestUrl\":\"/$REPOSITORY/pull/$PULL_REQUEST_NUMBER\"}" $AWS_GET_DEPLOYMENT_FUNC > found-deployment.json
-cat found-deployment.json
+scripts/bash/invoke-lambda-func.sh "{\"pullRequestUrl\":\"/$REPOSITORY/pull/$PULL_REQUEST_NUMBER\"}" $AWS_GET_DEPLOYMENT_FUNC
+cat outfile.json
