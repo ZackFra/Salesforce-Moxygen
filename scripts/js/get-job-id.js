156f586262fe470aad083435ebfc22feb645873e
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 19:20:39 -0800

    Refactor error handling in job validation to exit process on parse failure and throw error on validation failure

--- scripts/js/get-job-id.js
+++ scripts/js/get-job-id.js
@@ -25,7 +25,7 @@ function tryToParseValidateJob() {
         parseValidateJob(validateJobText);
     } catch (err) {
         console.error("Error parsing validate job: ", err);
-        fs.writeFileSync(FILE_NAME, "", "utf-8");
+        process.exit(1);
     }
 }
 
@@ -39,7 +39,7 @@ function parseValidateJob(validateJobText) {
         fs.writeFileSync(FILE_NAME, validateJob.status.result.id, "utf-8");
     } else {
         console.error("Job failed to validate");
-        process.exit(1);
+        throw new Error("Job failed to validate");
     }
 }
 
