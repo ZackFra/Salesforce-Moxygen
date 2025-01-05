54c2d470e80febc95aecd2f891748e24dc8b23dd
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 17:58:09 -0800

    Refactor AWS configuration in validate workflow to use environment variables for enhanced security

--- scripts/bash/config-aws.sh
+++ scripts/bash/config-aws.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npm i -g aws-cdk
-aws configure set aws_access_key_id $1
-aws configure set aws_secret_access_key $2
-aws configure set default.region $3 
+aws configure set aws_access_key_id $AWS_ACCESS_KEY
+aws configure set aws_secret_access_key $AWS_SECRET
+aws configure set default.region $AWS_REGION
 aws configure set default.output json
