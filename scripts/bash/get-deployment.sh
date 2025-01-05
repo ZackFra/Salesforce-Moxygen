c340a01fd1680317f21e8d6f02fb983ec4bc7d79
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-04 19:02:53 -0800

    Update get-deployment script to read Quick Deploy ID from job-id.txt instead of outfile.json

--- scripts/bash/get-deployment.sh
+++ scripts/bash/get-deployment.sh
@@ -2,5 +2,5 @@
 scripts/bash/config-aws.sh
 scripts/bash/invoke-lambda-func.sh "{\"pullRequestUrl\":\"/$REPOSITORY/pull/$PULL_REQUEST_NUMBER\"}" $AWS_GET_DEPLOYMENT_FUNC
 node scripts/js/get-job-id.js "outfile.json"
-$QUICK_DEPLOY_ID = $(cat "outfile.json")
+$QUICK_DEPLOY_ID = $(cat "job-id.txt")
 echo "Quick Deploy ID: $QUICK_DEPLOY_ID"
