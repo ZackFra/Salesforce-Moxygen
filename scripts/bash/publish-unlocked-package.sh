8e46337af97281f2eec5722e38d6295406b9a276
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-20 17:24:12 -0800

    [CHORE] Remove duplicate-files.json and duplicates.json; update publish-unlocked-package.sh for improved GitHub authentication

--- scripts/bash/publish-unlocked-package.sh
+++ scripts/bash/publish-unlocked-package.sh
@@ -9,6 +9,7 @@ app_bot_token=$2
 runner_bot_token=$3
 
 scripts/bash/config-github-bot.sh
+
 sf package version create --package Moxygen --installation-key-bypass --wait 10
 echo $app_bot_token | gh auth login --with-token
 git checkout -b $branch_name
@@ -16,6 +17,7 @@ git add sfdx-project.json
 git commit -m "[Automated] Created Package Version"
 git push --set-upstream origin $branch_name
 gh pr create --title "Publish Moxygen" --body "Automated PR to publish Moxygen" --base main
+
 echo $runner_bot_token | gh auth login --with-token
 gh pr review $branch_name -a
 echo $app_bot_token | gh auth login --with-token
