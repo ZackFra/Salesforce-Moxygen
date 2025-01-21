5606d986db1c1e3fd8eef93d36637037352e387c
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-21 15:19:48 -0800

    [FEATURE] Refactor Apex Docs generation into a separate script for improved maintainability

---
+++ scripts/bash/generate-apex-docs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+branch_name="apexdocs-$1"
+app_bot_token="$2"
+runner_bot_token="$3"
+
+scripts/bash/config-github-bot.sh
+
+npm i -g @cparra/apexdocs
+git checkout -b $branch_name
+rm ./docs -rf
+apexdocs markdown
+git add ./docs
+git commit -m "[Automated] Generate Apex Docs"
+git push --set-upstream origin $branch_name
+
+echo $app_bot_token | gh auth login --with-token
+gh pr create --title "Generate Apex Docs" --body "Automated PR to update Apex Docs" --base main
+
+echo $runner_bot_token | gh auth login --with-token
+gh pr review $branch_name -a
+
+echo $app_bot_token | gh auth login --with-token
+gh pr merge --squash --admin
