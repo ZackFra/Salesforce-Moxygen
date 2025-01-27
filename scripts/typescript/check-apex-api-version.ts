71775d6f2f1d64298398f303e29eab49c8970a7b
Zackary Frazier <zack11frazier22@gmail.com>
2025-01-26 22:32:44 -0800

    Fix query command syntax in Apex API version check script

--- scripts/typescript/check-apex-api-version.ts
+++ scripts/typescript/check-apex-api-version.ts
@@ -9,7 +9,7 @@ const orgDisplay = fs.readFileSync(path.resolve(process.cwd(), 'orgDisplay.json'
 const orgDisplayJson = JSON.parse(orgDisplay) as OrgDisplayResponse
 const apiVersion = parseInt(orgDisplayJson.result.apiVersion, 10)
 
-execSync(`sf data query --query "SELECT Name, ApiVersion FROM ApexClass WHERE ApiVersion != ${apiVersion} --json > query.json`)
+execSync(`sf data query --query "SELECT Name, ApiVersion FROM ApexClass WHERE ApiVersion != ${apiVersion}" --json > query.json`)
 
 const queryResponse = fs.readFileSync(path.resolve(process.cwd(), 'query.json'), 'utf-8')
 const queryResponseJson = JSON.parse(queryResponse) as QueryResponse
